28c82accacd925cf94a19a543f5da58fea5f1334
Arash <arashonquora@gmail.com>
2020-12-08 22:18:40 +0530

    updating

--- playground/some.js
+++ playground/some.js
@@ -1,1 +1,4 @@
-console.log("I'm in another File!")
+const p = document.querySelector("p");
+
+console.log("I'm in another File!");
+console.log(p);
---
+++ playground/types.js
@@ -0,0 +1,24 @@
+/* eslint-disable*/
+
+const name = "Arashvir";
+const middle = "Singh";
+const last = `Jamwal`;
+
+const age = 30;
+
+const sentence = "she's so cool!";
+
+const song = `ohh
+
+ya I 
+like 
+the 
+
+song`;
+
+const hello2 = `Hello my name is ${name} ${middle} ${last}
+}". Nice to meet you!". I am ${age + 100} years old`;
+
+const yo = 100.5;
+const a = 10;
+const b = 30;
