b11360190cad1edf1f105d637e2aa21f9c1f28dd
Arash <arashonquora@gmail.com>
2020-12-11 16:25:45 +0530

    incomplete work

--- playground/types.js
+++ playground/types.js
@@ -1,24 +1,43 @@
-/* eslint-disable*/
+// /* eslint-disable*/
+// Hello;
 
-const name = "Arashvir";
-const middle = "Singh";
-const last = `Jamwal`;
+// const name = "Arashvir";
+// const middle = "Singh";
+// const last = `Jamwal`;
 
-const age = 30;
+// const age = 30;
 
-const sentence = "she's so cool!";
+// const sentence = "she's so cool!";
 
-const song = `ohh
+// const song = `ohh
 
-ya I 
-like 
-the 
+// ya I
+// like
+// the
 
-song`;
+// song`;
 
-const hello2 = `Hello my name is ${name} ${middle} ${last}
-}". Nice to meet you!". I am ${age + 100} years old`;
+// const hello2 = `Hello my name is ${name} ${middle} ${last}
+// }". Nice to meet you!". I am ${age + 100} years old`;
 
-const yo = 100.5;
-const a = 10;
-const b = 30;
+// const yo = 100.5;
+// const a = 10;
+// const b = 30;
+
+// Number
+// typeof
+
+// const goodday = 20;
+// const family = 3;
+// const eachfamily = Math.floor(goodday / family);
+// console.log(`Each Kids get ${eachfamily}`);
+const person = {
+  first: "wes",
+  last: "bos",
+  old: 100,
+};
+
+// Everything is an Object in Javascript!
+// Order doesn't matter in js
+// Symbols are unique way of respresenting
+// undefined is default value before assigning anything
